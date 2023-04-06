19ff490e98b142ab2e97d33c4dccdc4dc55fa5e9
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-04-06 18:24:46 +0900

    feat: Guard 세팅

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ReqUser } from './auth.decorator';
 
 @Controller('auth')
 export class AuthController {
 
   @Post()
-  login(@Body() body: any) {
-    console.dir(body);
+  @UseGuards(AuthGuard('local'))
+  async login(@ReqUser() user: any) {
+    console.log(`ReqUser: `);
+    console.dir(user);
     return;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
